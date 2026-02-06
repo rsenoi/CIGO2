860f504f91cb8b8224a2f02cbd14bec9e7d76b74
rsenoi <ricardo.senoi@yahoo.com.br>
2026-02-06 15:52:38 -0300

    Acertando variaveis 3

--- database/db.go
+++ database/db.go
@@ -16,7 +16,7 @@ var (
 
 func ConectaComBancoDeDados() {
 	//stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable     "
-	stringDeConexao := "host=" + os.Getenv("HOST") + " user=" + os.Getenv("USER") + " password=" + os.Getenv("PASSWORD") + " dbname=" + os.Getenv("DBNAME") + " port=" + os.Getenv("PORT") + " sslmode=disable"
+	stringDeConexao := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " port=" + os.Getenv("DB_PORT") + " sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
